7debd9a637b6351aa0742dce08547991b8b0515f
Returnmakdo <cldud970@naver.com>
2023-03-22 10:39:05 +0900

    vue 기본문법 공부

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,66 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>이벤트 처리</h1>
+    <h1 v-if="bRender">bRender가 true면 이게 화면에 보임</h1>
+    <h1 v-else>bRender가 false면 이게 화면에 보임</h1>
+    <h1 v-if="type === 'A'">A</h1>
+    <h1 v-else-if="type === 'B'">B</h1>
+    <h1 v-else>C</h1>
+    <h1 v-show="bShow">bShow가 true면 이 블록이 화면에 보임</h1>
+    <button type="button" @click="increaseCounter">Add 1</button>
+    <p>현재 수 : {{ counter }}</p>
+    <button type="button" @click="setCount(7)">Add 1</button>
+    <p>현재 수 : {{ counter }}</p>
+    <select v-model="selectedValue" @change="changeSelect">
+      <option value="서울">서울</option>
+      <option value="부산">부산</option>
+      <option value="제주">제주</option>
+    </select>
+    <h1>Full Name : {{ fullName }}</h1>
+    <h1>My Name : {{ myName }}</h1>
+    <button type="button" @click="changeName">내 이름 나와라</button>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      type: "B",
+      bShow: true,
+      counter: 0,
+      selectedValue: "",
+      firstName: "Chiyoung",
+      lastName: "Ahn",
+      myName: "",
+    };
+  },
+  methods: {
+    increaseCounter() {
+      this.counter = this.counter + 1;
+    },
+    setCount(counter) {
+      this.counter = counter;
+    },
+    changeSelect() {
+      alert(this.selectedValue);
+    },
+    changeName() {
+      this.firstName = "ChiChi";
+    },
+  },
+  computed: {
+    fullName() {
+      return this.firstName + " " + this.lastName;
+    },
+  },
+  watch: {
+    firstName() {
+      this.myName = this.firstName + " " + this.lastName;
+    },
+    lastName() {
+      this.myName = this.firstName + " " + this.lastName;
+    },
+  },
+};
+</script>
--- src/views/DataBinding.vue
+++ src/views/DataBinding.vue
@@ -22,6 +22,39 @@
     <label><input type="radio" v-model="picked" v-bind:value="radioVaule3" />제주</label>
     <br />
     <span>체크한 지역 : {{ picked }}</span>
+    <br />
+    <img v-bind:src="imgSrc" />
+    <br />
+    <input type="text" v-model="textValue" />
+    <button type="button" v-bind:disabled="textValue === ''">Click</button><br />
+    <div
+      class="container"
+      v-bind:class="{
+        active: isActive,
+        'text-red': hasError,
+      }">
+      Class Binding
+    </div>
+    <div class="container" v-bind:class="[activeClass, errorClass]">Class Binding</div>
+    <div v-bind:style="styleObject">인라인 스타일 바인딩</div>
+    <table>
+      <thead>
+        <tr>
+          <th>제품명</th>
+          <th>가격</th>
+          <th>카테고리</th>
+          <th>배송료</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr :key="i" v-for="(product, i) in productList">
+          <td>{{ product.product_name }}</td>
+          <td>{{ product.price }}</td>
+          <td>{{ product.category }}</td>
+          <td>{{ product.delivery_price }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
@@ -39,7 +72,74 @@ export default {
       radioVaule1: "서울",
       radioVaule2: "부산",
       radioVaule3: "제주",
+      imgSrc: "https://kr.vuejs.org/images/logo.png",
+      textValue: "",
+      isActive: true,
+      hasError: false,
+      activeClass: "active",
+      errorClass: "text-red",
+      styleObject: {
+        color: "red",
+        fontSize: "13px",
+      },
+      productList: [
+        {
+          product_name: "기계식키보드",
+          price: 25000,
+          category: "노트북/태블릿",
+          delivery_price: 5000,
+        },
+        {
+          product_name: "무선마우스",
+          price: 12000,
+          category: "노트북/태블릿",
+          delivery_price: 5000,
+        },
+        {
+          product_name: "아이패드",
+          price: 725000,
+          category: "노트북/태블릿",
+          delivery_price: 5000,
+        },
+        {
+          product_name: "무선충전기",
+          price: 42000,
+          category: "노트북/태블릿",
+          delivery_price: 5000,
+        },
+        {
+          product_name: "태블릿거치대",
+          price: 32000,
+          category: "노트북/태블릿",
+          delivery_price: 5000,
+        },
+      ],
     };
   },
 };
 </script>
+
+<style scoped>
+container {
+  width: 100%;
+  height: 200px;
+}
+.active {
+  background-color: yellow;
+  font-weight: bold;
+}
+.text-red {
+  color: red;
+}
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+td,
+th {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+}
+</style>
